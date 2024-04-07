845366fceaee6186fdca6c05ffd1f6b866fa85bc
MariellaHeikkila <mariella.heikkila@gmail.com>
2024-04-07 15:23:32 +0300

    2.1. kurssitiedot step6 (refactoring osa1 kurssitiedot)

---
+++ osa02/kurssitiedot/src/App.jsx
@@ -0,0 +1,62 @@
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
+    </div>
+  )
+} 
+
+const Part = ({part, exercises}) => {
+  return (
+    <p>{part} {exercises}</p>
+  )
+}
