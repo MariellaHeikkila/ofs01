1d62242aefb304a825dff4e819dea5f48acb45d6
MariellaHeikkila <mariella.heikkila@gmail.com>
2024-05-11 11:49:00 +0300

    2.13: puhelinluettelo step8 (palvelinkommunikointi omaan moduuliin)

--- osa02/puhelinluettelo/README.md
+++ osa02/puhelinluettelo/README.md
@@ -1,2 +1,5 @@
 
 osan 2 tehtävien 2.6.-2.10. react-koodi tähän hakemistoon
+
+
+npx json-server --port=3001 --watch db.json
--- osa02/puhelinluettelo/src/App.jsx
+++ osa02/puhelinluettelo/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -8,11 +9,11 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response =>{
+    personService
+      .getAll()
+      .then(initialPersons =>{
         console.log('promise fulfilled')
-        setPersons(response.data)
+        setPersons(initialPersons)
       })
   }  , [])
 
@@ -28,10 +29,10 @@ const App = () => {
       return
     }
 
-    axios
-      .post('http://localhost:3001/persons', nameObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    personService
+      .create(nameObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
       })
